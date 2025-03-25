c4e79950c17adb0e554ec5b702fa230942252181
Saikrishna Mada <sai.mada@testingxperts.com>
2025-03-25 15:06:12 +0530

    Added Demo

---
+++ Test Suites/MyDemo/TestSuite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c4f6af18-f11d-42cd-99c9-a07a58fb6ad0</testSuiteGuid>
+</TestSuiteEntity>
