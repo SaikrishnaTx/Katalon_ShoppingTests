0b8812d1778e9002bf53d70d066ac6c50edb8bce
Saikrishna Mada <sai.mada@testingxperts.com>
2025-03-25 13:54:48 +0530

    Added DPP Login

---
+++ Test Suites/DPP/DPP_Test.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DPP_Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>dc8812b0-4941-4713-9140-ba707b63ac35</testSuiteGuid>
+   <testCaseLink>
+      <guid>a2f68a51-803e-4ae6-a44b-4f50b911fc6b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Deluxe/Deluxe_Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
